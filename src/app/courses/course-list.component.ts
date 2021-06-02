f57bb9676c0cfafc4c59722a41c2c60baff1b9a3
Paulo-UNIRON <paulo.fonseca@uniron.edu.br>
2021-06-01 22:04:47 -0400

    Primeiro commit

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({    
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+    // template: '<h2> Course List </h2>'
+    
+})
+
+export class CourseListComponent {
+    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+    //Variável com _ serve par indicar que só vai ser usada aqui
+
+    _courses: Course[] = [];
+    _filterBy!: string;
+
+    constructor(private courseservice: CourseService){
+    }
+
+    ngOnInit(): void{
+        this._courses = this.courseservice.retrieveAll();
+        this.filteredCourses = this._courses;
+       }
+
+    set filter(value: string) { 
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+       }
+
+    get filter() { 
+        return this._filterBy;
+    }       
+}
